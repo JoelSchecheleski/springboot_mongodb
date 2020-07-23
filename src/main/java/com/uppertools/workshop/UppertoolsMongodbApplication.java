f320798321e866babf03d23155f7b5cac2978191
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-07-23 14:25:36 -0300

    Project init

---
+++ src/main/java/com/uppertools/workshop/UppertoolsMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.uppertools.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppertoolsMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppertoolsMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uppertools/workshop/UppertoolsMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uppertools.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppertoolsMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
