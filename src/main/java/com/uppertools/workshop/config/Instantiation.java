5ed07e316d0caebb0c85bdfa69f4e4e0aa77468f
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-07-24 08:44:57 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/uppertools/workshop/config/Instantiation.java
+++ src/main/java/com/uppertools/workshop/config/Instantiation.java
@@ -1,7 +1,11 @@
 package com.uppertools.workshop.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
+import com.uppertools.workshop.domain.Post;
+import com.uppertools.workshop.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -14,16 +18,27 @@ public class Instantiation implements CommandLineRunner{
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
-		userRepository.deleteAll();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		this.userRepository.deleteAll();
 		User u1 = new User(null, "Joel Schecheleski", "joel.jsp@gmail.com");
 		User u2 = new User(null, "Kátia Michele Jurk Avalos Schecheleski", "kmjavalos@gmail.com");
 		User u3 = new User(null, "Miguel Schecheleski", "miguel@gmail.com");
 		User u4 = new User(null, "Raquel Schecheleski", "raquel@gmail.com");
-		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
+		this.userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
+
+		this.postRepository.deleteAll();
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", u1);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", u1);
+		this.postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
 
---
+++ src/main/java/com/uppertools/workshop/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.uppertools.workshop.repository;
+
+import com.uppertools.workshop.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
